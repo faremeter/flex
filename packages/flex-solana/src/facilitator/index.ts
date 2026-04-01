4553b351a5c4a3eb0595580bbe69a285dd0b77e9
FarinM <farin681@gmail.com>
2026-04-01 22:23:02 +0200

    Trim internal symbols from flex-solana public API

--- packages/flex-solana/src/facilitator/index.ts
+++ packages/flex-solana/src/facilitator/index.ts
@@ -2,10 +2,3 @@ export { createFacilitatorHandler } from "./handler";
 export type { FlexFacilitator, FlushResult } from "./handler";
 export { fetchEscrowAccounting } from "./accounting";
 export type { HoldEntry, EscrowAccounting } from "./accounting";
-export { createHoldManager } from "./hold-manager";
-export type {
-  Hold,
-  HoldManager,
-  TryHoldParams,
-  HoldResult,
-} from "./hold-manager";
