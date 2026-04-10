e8bfe7ef11c26d339ecf3e78a19c4691a7b2fd6c
FarinM <farin681@gmail.com>
2026-04-10 09:45:08 +0200

    Accept upto as a valid scheme alongside @faremeter/flex

--- packages/flex-solana/src/scheme.ts
+++ packages/flex-solana/src/scheme.ts
@@ -1,2 +1,5 @@
 /** Scheme identifier used in x402 payment requirements for Flex. */
 export const FLEX_SCHEME = "@faremeter/flex";
+
+/** x402 standard "upto" scheme, settled via Flex. */
+export const UPTO_SCHEME = "upto";
